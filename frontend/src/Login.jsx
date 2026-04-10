77bcee9bc5de413f8d4eb0d8dbed12f237f06c45
William Brady <bradlywilliam3@gmail.com>
2026-04-11 00:19:15 +0100

    connect backend api

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://bank-mvp.onrender.com/api/auth/login", {
         email,
         password,
       });
