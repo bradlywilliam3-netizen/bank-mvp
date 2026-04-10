a4c1b169c2630b94241282280bf0c318e4284414
William Brady <bradlywilliam3@gmail.com>
2026-04-10 23:08:19 +0100

    fix mongo + port

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,8 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/transactions', require('./routes/transactions'));
 
 // start server
-app.listen(5000, () => {
-  console.log('Server running on port 5000 🚀');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
